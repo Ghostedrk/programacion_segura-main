f70ccead02a552076affe86ee7916a5cd987c3da
Ghostedrk <darkghost2189976@gmail.com>
2025-07-22 18:16:07 -0400

    22/07

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # AP-173-N1
 
-Clases de introducción a la programación
+<h1>Clases de introducción a la programación</1>
 
-Sección AP-173-N1
+<h3>Sección AP-173-N1</h3>
+
+<ul>
+<li>Versión de MysqlConnector para Python 3.11
+<p><code>pip install mysql-connector-python==8.0.33</code></p></li>
+<li>prettytable 3.16.0
+<p><code>pip install prettytable</code></p></li>
+</ul>
--- test.py
+++ test.py
@@ -1,7 +1,49 @@
-from ejercicios.ejercicio_1 import convertidor_temp
+# from ejercicios.ejercicio_1 import convertidor_temp
 
-temp = float(input("Ingrese su temperatura a convertir: "))
-escala_inicial = input("Indique escala inicial: C, F o K: ").upper().strip()
-escala_final = input("Indique escala final: C, F o K: ").upper().strip()
+# temp = float(input("Ingrese su temperatura a convertir: "))
+# escala_inicial = input("Indique escala inicial: C, F o K: ").upper().strip()
+# escala_final = input("Indique escala final: C, F o K: ").upper().strip()
 
-convertidor_temp(temp,escala_inicial,escala_final)
+# convertidor_temp(temp,escala_inicial,escala_final)
+# lista_comidas = ['sopaipillas','charquican','empanadas']
+# print(lista_comidas[2])
+# lista_comidas[2] = 'Empanadas fritas'
+# print(lista_comidas)
+# for i in range(5):
+#     print(i)
+
+# def division(a,b):
+#     resultado = a/b
+#     return resultado
+
+# numero1_str = input('Ingrese número:')
+# numero2_str = input('Ingrese número:')
+# numero1 = 0
+# numero2 = 0
+
+# if numero1_str.isdigit():
+#     numero1 = int(numero1_str)
+# if numero2_str.isdigit():
+#     numero2 = int(numero2_str)
+
+# division(numero1,numero2)
+
+# cadena_test = '''
+# Mi corazón, ni una ni dos veces se rompió.
+# Sus bolsillos se vaciaron por las necesidades de ambos.
+# Un control lo suficientemente fuerte no podría contener sus finanzas.
+# Sobre la dignidad o la luz que una vez hubo en mi alma
+# Desde la primera compra mi corazón no aprendió
+# Tomó prestado de otros lugares para poder ganar dinero.
+# El precio que pediste a cambio por tu amor.
+# El precio que pediste a cambio por tu amor.
+# Mi corazón, ni una ni dos veces se rompió.
+# En quiebra, agotados, sólo entonces para tomar nota
+# Que por error había utilizado mala economía
+# En la compra del amor, que debería haber venido gratis
+# '''
+try:
+    valor = int(input('ingrese un entero: '))
+    print(valor)
+except:
+    print('Valor incorrecto')
